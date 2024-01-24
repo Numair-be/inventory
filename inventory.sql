62198d429fb9973b795a2747f9ceb2050e25549f
Numair-be <numairstriker@gmail.com>
2024-01-24 11:05:05 +0530

    schema

---
+++ inventory.sql
@@ -0,0 +1,34 @@
+CREATE TABLE customer(
+    customer_id serial primary key,
+    customer_name varchar(100),
+    age numeric check (age>0 and age<100),
+    email varchar(100) unique,
+    cust_date timestamp default current_timestamp
+);
+
+CREATE TABLE product(
+    product_id serial primary key,
+    product_name varchar(100)
+);
+
+CREATE TABLE inventory(
+    inventory_id serial primary key,
+    product_id integer references product(product_id),
+    quantity numeric check(quantity>=0),
+    inventory_update timestamp default current_timestamp
+);
+
+CREATE TABLE INVOICE(
+    invoive_id serial primary key,
+    customer_id integer references customer(customer_id),
+    product_id integer references product(product_id),
+    quantity integer check (quantity>=0),
+    price integer check (price>0),
+    inv_date timestamp default current_timestamp
+);
+
+insert into customer (customer_name, age, email) 
+values('virat',35,'virat@gmail.com');
+
+select * from customer;
+select customer_name,age from customer;
